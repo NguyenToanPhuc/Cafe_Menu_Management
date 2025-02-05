ca435bab06f20b1b50079457eed5106d145d7c97
Toàn Phúc Nguyễn <phucnguyentenda@gmail.com>
2025-02-05 22:14:15 +0700

    Fixing Recipe hpp và cpp

--- header/Recipe.hpp
+++ header/Recipe.hpp
@@ -3,23 +3,27 @@
 #define RECIPE_HPP
 #include "Library.h"
 class Ingredient {
-	private:
-		string name;
-		int Amount;
-		bool Unit; //1 la g 0 la ml
-	public:
-		Ingredient();
-		Ingredient(string name_, int Amount_, bool Unit_);
-		void Insert(string name_, int Amount_, bool Unit_);
+private:
+	string name;
+	int Amount;
+	bool Unit; //1 la g 0 la ml
+public:
+	Ingredient();
+	Ingredient(string name_, int Amount_, bool Unit_);
+	void Insert(string name_, int Amount_, bool Unit_);
 };
 typedef Recipe* PTR_RECIPE;
-class Recipe {
+class RecipeNode {
 	Ingredient data;
 	Recipe* next;
 
-	Recipe();
+	RecipeNode();
 	PTR_RECIPE New_Node(Ingredient data_);
 	void Insert_Head(Ingredient data_, PTR_RECIPE first);
 };
+class Recipe {
+private:
+	Recipe
 
+};
 #endif RECIPE_HPP
--- src/Recipe.cpp
+++ src/Recipe.cpp
@@ -1,29 +1,38 @@
-#include "../include/Recipe.hpp"
+#include "../header/Recipe.hpp"
 
-Ingredient::Ingredient() {};
-
-Ingredient::Ingredient(string name_, int Amount_, bool Unit_) {
-    this->name = name_;
-    this->Amount = Amount_;
-    this->Unit = Unit_;
+RecipeNode::RecipeNode(const Ingredient _data) {
+	data = _data;
+	next = nullptr;
 }
 
-void Ingredient::Insert(string name_, int Amount_, bool Unit_) {
-    name = name_;
-    Amount = Amount_;
-    Unit = Unit_;
+//------------------------------------------------/
+Recipe::Recipe() {
+	head = nullptr;
 }
 
-Recipe::Recipe() : data(), next(nullptr) {};
-
-PTR_RECIPE Recipe::New_Node(Ingredient data_) {
-    PTR_RECIPE temp = new Recipe;
-    temp->data = data_;
-
+Recipe::~Recipe() {
+	while (!Empty()) {
+		Remove();
+	}
 }
-void Recipe::Insert_Head(Ingredient data_, PTR_RECIPE first) {
-    PTR_RECIPE temp = new Recipe;
-    temp->data = data_;
-    temp->next = first;
-    first = temp;
+
+bool Recipe::Empty() const {
+	if (head == nullptr) {
+		return true;
+	}
+	return false;
+}
+
+void Recipe::AddFront(const Ingredient _data)
+{
+	RecipeNode* NewNode = new RecipeNode(_data);
+	NewNode->next = head;
+	head = NewNode;
+}
+
+void Recipe::RemoveFront()
+{
+	RecipeNode* temp = head;
+	head = temp->next;
+	delete temp;
 }
